d8b6b7d07ad41094de6059ae7620737510fae59b
cabbage_cultist <60555764+cabbagecultist@users.noreply.github.com>
2024-06-23 00:22:32 -0400

    Fixed HTTPS Certs and requests being made constantly

--- src/Digital_Clock.cpp
+++ src/Digital_Clock.cpp
@@ -20,39 +20,38 @@ RTC_DS3231 rtc;
 const String serverAddress = "https://splatoon3.ink/data/schedules.json";
 JsonDocument latestData;
 
-// const char* root_ca = \ 
-// "-----BEGIN CERTIFICATE-----\n" \
-// "MIIFcDCCBFigAwIBAgIRAPsU9FnGLEVVDXsSKUzwsuYwDQYJKoZIhvcNAQELBQAw\n" \
-// "RjELMAkGA1UEBhMCVVMxIjAgBgNVBAoTGUdvb2dsZSBUcnVzdCBTZXJ2aWNlcyBM\n" \
-// "TEMxEzARBgNVBAMTCkdUUyBDQSAxUDUwHhcNMjQwNTI5MTIxOTIwWhcNMjQwODI3\n" \
-// "MTIxOTE5WjAYMRYwFAYDVQQDEw1zcGxhdG9vbjMuaW5rMIIBIjANBgkqhkiG9w0B\n" \
-// "AQEFAAOCAQ8AMIIBCgKCAQEA6KHnTRdQFx6RdgKxjgF8lHLluGm4gscGqr325wFy\n" \
-// "zXHfVYOxnbmvq0b3LFoJ0sCCbhXwg84gSwctkaYsmsGcXpdqfIOMjGdHMrzmh+fo\n" \
-// "o0o419ImNe44hoDdv4ORHQ0rkSpa7+KMu41StKWoxR+t9gyeLAGFWpAaWTYl47dD\n" \
-// "BmXc8Q+/5maEsRgLxLGT+LU6nL1tReyhH/9/KKymXKEOg081BhnVHl4qFa+3XYLn\n" \
-// "wwtTru3lrQhuUe3SrzlNoexMQQ2x62OYZprG0fH4FRlBvs7mrsoTF1sLEmqwejkU\n" \
-// "yJRQTOdEb0k+40XjMEdP9WGhxcCxJe9EEOLdkYTs8iyODwIDAQABo4IChTCCAoEw\n" \
-// "DgYDVR0PAQH/BAQDAgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMBMAwGA1UdEwEB/wQC\n" \
-// "MAAwHQYDVR0OBBYEFEiWme0RrWYnS/uuZvhKQZKXsKkmMB8GA1UdIwQYMBaAFNX8\n" \
-// "ng3fHsrdCJeXbivFX8Ur9ey4MHgGCCsGAQUFBwEBBGwwajA1BggrBgEFBQcwAYYp\n" \
-// "aHR0cDovL29jc3AucGtpLmdvb2cvcy9ndHMxcDUvMExLOEpXZHp1YTQwMQYIKwYB\n" \
-// "BQUHMAKGJWh0dHA6Ly9wa2kuZ29vZy9yZXBvL2NlcnRzL2d0czFwNS5kZXIwKQYD\n" \
-// "VR0RBCIwIIINc3BsYXRvb24zLmlua4IPKi5zcGxhdG9vbjMuaW5rMCEGA1UdIAQa\n" \
-// "MBgwCAYGZ4EMAQIBMAwGCisGAQQB1nkCBQMwPAYDVR0fBDUwMzAxoC+gLYYraHR0\n" \
-// "cDovL2NybHMucGtpLmdvb2cvZ3RzMXA1LzU4S293N2w1QVBrLmNybDCCAQQGCisG\n" \
-// "AQQB1nkCBAIEgfUEgfIA8AB1AO7N0GTV2xrOxVy3nbTNE6Iyh0Z8vOzew1FIWUZx\n" \
-// "H7WbAAABj8SBVdkAAAQDAEYwRAIgbmIeyfTyLEHhPgFJb6ZDafkoUzmqL1t01MXW\n" \
-// "27dAT58CIGzDsP0jwWknpkeeuLDxPS4PAIb7YB7VC+5KB0PnBeG0AHcASLDja9qm\n" \
-// "RzQP5WoC+p0w6xxSActW3SyB2bu/qznYhHMAAAGPxIFV/QAABAMASDBGAiEAvux1\n" \
-// "a3kFogFpooXoGwK0TZbfHEYNZ2r1xGkbsEtx7U0CIQDx6fIg+OhO+NVwwd2LrddD\n" \
-// "hIj3z1TdxPQTFnrFXTZjgTANBgkqhkiG9w0BAQsFAAOCAQEARXWZzfdRz4CLCpxc\n" \
-// "9ymVNKXUW+puaKMzP+Vi+lAw3Sa+o4nnSq4VL3qMQOA5mPR+hFCLvrAf5YLBjxoE\n" \
-// "Z5vT1/WXhswlzxu8hkD9jT1hUtDwdWv1KhRPm3YZRZ9TUKLO1xG2D3vEAqbtIWmJ\n" \
-// "/ZjXKQpoqbttuEJN8+2ERXFci2we9nOqGQImctVgoa3oNTyg1asWGQ7Y+JWt6A/P\n" \
-// "KAUPVb+pl3fK9jpjjd55FqfwAZOOVWlkDeLDB6Gpg3ZOZ1A82cHb2uTQoR7ha4oy\n" \
-// "wMniwTQIGuBOZ+DDvgQ/tWaIy1f7482cqUun4lVgmiZS2172nNvnciKi3pE3FAfz\n" \
-// "f3Ew7Q==\n" \
-// "-----END CERTIFICATE-----\n";
+const char* root_ca = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIFYjCCBEqgAwIBAgIQd70NbNs2+RrqIQ/E8FjTDTANBgkqhkiG9w0BAQsFADBX\n" \
+"MQswCQYDVQQGEwJCRTEZMBcGA1UEChMQR2xvYmFsU2lnbiBudi1zYTEQMA4GA1UE\n" \
+"CxMHUm9vdCBDQTEbMBkGA1UEAxMSR2xvYmFsU2lnbiBSb290IENBMB4XDTIwMDYx\n" \
+"OTAwMDA0MloXDTI4MDEyODAwMDA0MlowRzELMAkGA1UEBhMCVVMxIjAgBgNVBAoT\n" \
+"GUdvb2dsZSBUcnVzdCBTZXJ2aWNlcyBMTEMxFDASBgNVBAMTC0dUUyBSb290IFIx\n" \
+"MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAthECix7joXebO9y/lD63\n" \
+"ladAPKH9gvl9MgaCcfb2jH/76Nu8ai6Xl6OMS/kr9rH5zoQdsfnFl97vufKj6bwS\n" \
+"iV6nqlKr+CMny6SxnGPb15l+8Ape62im9MZaRw1NEDPjTrETo8gYbEvs/AmQ351k\n" \
+"KSUjB6G00j0uYODP0gmHu81I8E3CwnqIiru6z1kZ1q+PsAewnjHxgsHA3y6mbWwZ\n" \
+"DrXYfiYaRQM9sHmklCitD38m5agI/pboPGiUU+6DOogrFZYJsuB6jC511pzrp1Zk\n" \
+"j5ZPaK49l8KEj8C8QMALXL32h7M1bKwYUH+E4EzNktMg6TO8UpmvMrUpsyUqtEj5\n" \
+"cuHKZPfmghCN6J3Cioj6OGaK/GP5Afl4/Xtcd/p2h/rs37EOeZVXtL0m79YB0esW\n" \
+"CruOC7XFxYpVq9Os6pFLKcwZpDIlTirxZUTQAs6qzkm06p98g7BAe+dDq6dso499\n" \
+"iYH6TKX/1Y7DzkvgtdizjkXPdsDtQCv9Uw+wp9U7DbGKogPeMa3Md+pvez7W35Ei\n" \
+"Eua++tgy/BBjFFFy3l3WFpO9KWgz7zpm7AeKJt8T11dleCfeXkkUAKIAf5qoIbap\n" \
+"sZWwpbkNFhHax2xIPEDgfg1azVY80ZcFuctL7TlLnMQ/0lUTbiSw1nH69MG6zO0b\n" \
+"9f6BQdgAmD06yK56mDcYBZUCAwEAAaOCATgwggE0MA4GA1UdDwEB/wQEAwIBhjAP\n" \
+"BgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBTkrysmcRorSCeFL1JmLO/wiRNxPjAf\n" \
+"BgNVHSMEGDAWgBRge2YaRQ2XyolQL30EzTSo//z9SzBgBggrBgEFBQcBAQRUMFIw\n" \
+"JQYIKwYBBQUHMAGGGWh0dHA6Ly9vY3NwLnBraS5nb29nL2dzcjEwKQYIKwYBBQUH\n" \
+"MAKGHWh0dHA6Ly9wa2kuZ29vZy9nc3IxL2dzcjEuY3J0MDIGA1UdHwQrMCkwJ6Al\n" \
+"oCOGIWh0dHA6Ly9jcmwucGtpLmdvb2cvZ3NyMS9nc3IxLmNybDA7BgNVHSAENDAy\n" \
+"MAgGBmeBDAECATAIBgZngQwBAgIwDQYLKwYBBAHWeQIFAwIwDQYLKwYBBAHWeQIF\n" \
+"AwMwDQYJKoZIhvcNAQELBQADggEBADSkHrEoo9C0dhemMXoh6dFSPsjbdBZBiLg9\n" \
+"NR3t5P+T4Vxfq7vqfM/b5A3Ri1fyJm9bvhdGaJQ3b2t6yMAYN/olUazsaL+yyEn9\n" \
+"WprKASOshIArAoyZl+tJaox118fessmXn1hIVw41oeQa1v1vg4Fv74zPl6/AhSrw\n" \
+"9U5pCZEt4Wi4wStz6dTZ/CLANx8LZh1J7QJVj2fhMtfTJr9w4z30Z209fOU0iOMy\n" \
+"+qduBmpvvYuR7hZL6Dupszfnw0Skfths18dG9ZKb59UhvmaSGZRVbNQpsg3BZlvi\n" \
+"d0lIKO2d1xozclOzgjXPYovJJIultzkMu34qQb9Sz/yilrbCgj8=\n" \
+"-----END CERTIFICATE-----\n";
 
 bool connected = false;
 bool WiFiTimeout = false;
@@ -74,8 +73,8 @@ JsonDocument deserialize(String json) {
 String httpsGETRequest(const String serverAddress) {
   WiFiClientSecure client;
   HTTPClient https;
-  client.setInsecure();
-  // client.setCACert(root_ca);
+  // client.setInsecure();
+  client.setCACert(root_ca);
   https.begin(client, serverAddress);
   int httpResponseCode = https.GET();
   String response = "{}";
@@ -126,8 +125,15 @@ void loop() {
 
   //Code requiring an internet connection
   if (connected) {
-    // Make a GET request every 30m
-    if ((millis() - lastTime) > 1,8e+6) {
+    // Make a GET request once, then repeat every 30m
+    static bool firstRequest = false;
+    if (!firstRequest) {
+      String response = httpsGETRequest(serverAddress);
+      latestData = deserialize(response);
+      firstRequest = true;
+    }
+
+    if ((millis() - lastTime) > 18000000) {
       String response = httpsGETRequest(serverAddress);
       latestData = deserialize(response);
       lastTime = millis();
